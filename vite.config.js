8aa17e1100fc5a367fb4c27b2cb403a791216bdb
曹冲 <1909568816@qq.com>
2026-07-08 12:16:43 +0800

    build: add base path for gitee pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/wallpaper-browser/',
   server: {
     port: 5173,
     host: true
+  },
+  build: {
+    outDir: 'dist'
   }
 })
