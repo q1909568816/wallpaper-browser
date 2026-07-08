09553e7091c47aa4e809b9604ae996c5ddf77413
曹冲 <1909568816@qq.com>
2026-07-08 12:36:54 +0800

    ci: add github pages workflow

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/wallpaper-browser/',
   server: {
     port: 5173,
     host: true
